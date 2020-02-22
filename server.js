27f83e5331b4962ee4d1cfbb5af55bd4d4588b45
LeoFdz <lfernandez79@github.com>
2020-02-22 12:42:32 -0600

    added comment to server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+// Create server and port
 const express = require("express");
 const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Set to JSON all incoming
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
@@ -11,6 +13,8 @@ app.use(express.static("public"));
 require("./routes/apiRoutes")(app)
 require("./routes/htmlRoutes")(app)
 
+
+// Server starts listening 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
